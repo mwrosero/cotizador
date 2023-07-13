60105b28484a97f0072f196b94c4737bc2ccdf15
Michael Rosero <michael.rosero@akold.com>
2023-07-13 10:58:24 -0500

    Ajuste visual

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -101,7 +101,7 @@
                     <!-- Content wrapper -->
                     <div class="content-wrapper">
                         <!-- Content -->
-                        <div class="container-xxl flex-grow-1 container-p-y pt-0">
+                        <div class="container-xxl flex-grow-1 container-p-y">
                             @yield('content')
                         </div>
                         <!-- / Content -->
