64828b274276d67432419d820e3d8510e7dbaca3
Michael Rosero <michael.rosero@akold.com>
2023-10-25 11:49:14 -0500

    Usuario TUSC

--- resources/views/cotizador/cotizaciones.blade.php
+++ resources/views/cotizador/cotizaciones.blade.php
@@ -98,7 +98,7 @@
                             </tr>
                         @endif
                         @foreach ($datosPaginados as $dato)
-                            @if( session('userData')->codigoUsuario == $dato->usuarioIngreso || session('userData')->codigoUsuario == "MVELEZ" || session('userData')->codigoUsuario == "DREVELO" || session('userData')->codigoUsuario == "SMERIZALDE" )
+                            @if( session('userData')->codigoUsuario == $dato->usuarioIngreso || session('userData')->codigoUsuario == "MVELEZ" || session('userData')->codigoUsuario == "DREVELO" || session('userData')->codigoUsuario == "SMERIZALDE" || session('userData')->codigoUsuario == "TUSCOCOVICH")
                             <tr class="fs-12">
                                 <td>{{ $dato->idCotizacion }}</td>
                                 <td>{{ $dato->nombreCliente }}</td>
